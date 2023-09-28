ce654f80bb58d4cbee00e1a9a8643c1da893634a
Ben Young <developing.ben@gmail.com>
2023-09-28 13:46:04 -0500

    flushing out goal text input

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { Button, StyleSheet, TextInput, View, Text } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoalText);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="Please enter a goal!"
+          onChangeText={goalInputHandler}
         />
-        <Button title="Add Goal" />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
+      <View style={styles.goalsList}>
         <Text>List of Goals ...</Text>
       </View>
     </View>
@@ -19,18 +31,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 75,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     marginTop: 20,
-    flexDirection: "column",
+    flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 2,
+    borderBottomColor: "gray",
   },
   textInput: {
     borderColor: "darksalmon",
     borderWidth: 2,
     borderRadius: 5,
+    width: "80%",
     padding: 10,
+  },
+  goalsList: {
+    flex: 6,
   },
 });
