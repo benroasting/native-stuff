6d1adaee58fc5063a91046e15ebf2d73ae7357a6
Ben Young <developing.ben@gmail.com>
2023-11-22 11:12:19 -0600

    setup for the game screen

--- NativeGame/screens/GameScreen.js
+++ NativeGame/screens/GameScreen.js
@@ -1,7 +1,25 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import CustomButton from "../components/CustomButton";
 
 function GameScreen() {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Higher or Lower?</Text>
+        <CustomButton>LOWER</CustomButton>
+        <CustomButton>HIGHER</CustomButton>
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
