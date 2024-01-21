976799ae57f37ef5b9288e8e1147b1bb99c7841c
Ben Young <developing.ben@gmail.com>
2024-01-21 15:57:42 -0600

    starting MealTime app -- work on navigation

---
+++ MealTime/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MealCategoryScreen from "./screens/MealCategoryScreen";
+
+export default function App() {
+  return <MealCategoryScreen />;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
